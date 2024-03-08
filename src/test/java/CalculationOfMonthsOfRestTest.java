776a0b59f0c02e77957cc450e502da0c772d8bc9
Katya <dolgova01011987@gmail.com>
2024-03-08 16:57:17 +0300

    Initiall commit

---
+++ src/test/java/CalculationOfMonthsOfRestTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.services.CalculationOfMonthsOfRest;
+
+
+public class CalculationOfMonthsOfRestTest {
+    @Test
+    public void threeMonthsAYear() {
+        CalculationOfMonthsOfRest service = new CalculationOfMonthsOfRest();
+        int expected = 3;
+        int actual = service.calculate(10_000, 3_000, 20_000);
+
+
+    }
+
+    @Test
+    public void twoMonthsAYear() {
+        CalculationOfMonthsOfRest service = new CalculationOfMonthsOfRest();
+        int expected = 2;
+        int actual = service.calculate(100_000, 60_000, 150_000);
+
+
+    }
+
+
+}
